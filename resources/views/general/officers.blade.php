42a9d5d2c578ef9e10a79165620a331505317126
FGSERVER <jonatanvilella@gmail.com>
2023-04-26 22:46:35 -0300

    Oficiales Topnav (parte 1)
    Oficiales Topnav (parte 1)

--- resources/views/general/officers.blade.php
+++ resources/views/general/officers.blade.php
@@ -1,26 +1,28 @@
 <div id="officers" class="  fright">
-    <a href="https://s145-ar.ogproyect.gameforge.com/game/index.php?page=premium&amp;openDetail=2"
-        class="tooltipHTML   commander js_hideTipOnMobile "
-        title="Contratar comandante|+40 favoritos, lista de construcción, accesos rápidos, escáner de transportes, sin publicidad*&lt;span style=&quot;font-size:10px;line-height:10px;&quot;&gt;(*excepto mensajes sobre el juego)&lt;/span&gt;">
-        <img src="{{ $img_path . 'layout/pixel.gif' }}" width="30" height="30"></img>
+    <a href="game.php?page=premium&amp;openDetail=2"
+        class="tooltipHTML {{ $short_time_officier_commander }} {{ $img_premium_officier_commander }} commander js_hideTipOnMobile "
+        title="{{ $of_hire_commander }}|{{ $add_premium_officier_commander }}">
+        <img src="{{ $img_path . 'layout/pixel.gif' }}" width="30" height="30" />
     </a>
-    <a href="https://s145-ar.ogproyect.gameforge.com/game/index.php?page=premium&amp;openDetail=3"
-        class="tooltipHTML    admiral js_hideTipOnMobile "
-        title="Contratar almirante|Huecos de flota máximos +2, tasa de retirada de flotas mejorada">
-        <img src="{{ $img_path . 'layout/pixel.gif' }}" width="30" height="30"></img>
+    <a href="game.php?page=premium&amp;openDetail=3"
+        class="tooltipHTML {{ $short_time_officier_admiral }} {{ $img_premium_officier_admiral }} admiral js_hideTipOnMobile "
+        title="{{ $of_hire_admiral }}|{{ $add_premium_officier_admiral }}">
+        <img src="{{ $img_path . 'layout/pixel.gif' }}" width="30" height="30" />
     </a>
-    <a href="https://s145-ar.ogproyect.gameforge.com/game/index.php?page=premium&amp;openDetail=4"
-        class="tooltipHTML    engineer js_hideTipOnMobile "
-        title="Contratar ingeniero|Recorta a la mitad las perdidas de defensa, +10% en la producción de energia">
-        <img src="{{ $img_path . 'layout/pixel.gif' }}" width="30" height="30"></img>
+    <a href="game.php?page=premium&amp;openDetail=4"
+        class="tooltipHTML {{ $short_time_officier_engineer }} {{ $img_premium_officier_engineer }} engineer js_hideTipOnMobile "
+        title="{{ $of_hire_engineer }}|{{ $add_premium_officier_engineer }}">
+        <img src="{{ $img_path . 'layout/pixel.gif' }}" width="30" height="30" />
     </a>
-    <a href="https://s145-ar.ogproyect.gameforge.com/game/index.php?page=premium&amp;openDetail=5"
-        class="tooltipHTML    geologist js_hideTipOnMobile " title="Contratar geólogo|+10% producción de mineral">
-        <img src="{{ $img_path . 'layout/pixel.gif' }}" width="30" height="30"></img>
+    <a href="game.php?page=premium&amp;openDetail=5"
+        class="tooltipHTML {{ $short_time_officier_geologist }} {{ $img_premium_officier_geologist }} geologist js_hideTipOnMobile "
+        title="{{ $of_hire_geologist }}|{{ $add_premium_officier_geologist }}">
+        <img src="{{ $img_path . 'layout/pixel.gif' }}" width="30" height="30" />
     </a>
-    <a href="https://s145-ar.ogproyect.gameforge.com/game/index.php?page=premium&amp;openDetail=6"
-        class="tooltipHTML    technocrat js_hideTipOnMobile "
-        title="Contratar bureocrata|+2 al nivel de espionaje, 25% menos tiempo de investigación">
-        <img src="{{ $img_path . 'layout/pixel.gif' }}" width="30" height="30"></img>
+    <a href="game.php?page=premium&amp;openDetail=6"
+        class="tooltipHTML {{ $short_time_officier_technocrat }} {{ $img_premium_officier_technocrat }}  technocrat js_hideTipOnMobile "
+        title="{{ $of_hire_technocrat }}|{{ $add_premium_officier_technocrat }}">
+        <img src="{{ $img_path . 'layout/pixel.gif' }}" width="30" height="30" />
     </a>
 </div>
+{{ $short_time_officier_technocrat }}
