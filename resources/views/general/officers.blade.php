4d2b12ba2714ddba0285215c0865cc36a8c1e1ca
FGSERVER <jonatanvilella@gmail.com>
2023-04-26 23:21:21 -0300

    Fix template oficiales

--- resources/views/general/officers.blade.php
+++ resources/views/general/officers.blade.php
@@ -1,28 +1,27 @@
 <div id="officers" class="  fright">
     <a href="game.php?page=premium&amp;openDetail=2"
-        class="tooltipHTML {{ $short_time_officier_commander }} {{ $img_premium_officier_commander }} commander js_hideTipOnMobile "
+        class="tooltipHTML {{ $end_premium_officier_commander }} {{ $img_premium_officier_commander }} commander js_hideTipOnMobile "
         title="{{ $of_hire_commander }}|{{ $add_premium_officier_commander }}">
         <img src="{{ $img_path . 'layout/pixel.gif' }}" width="30" height="30" />
     </a>
     <a href="game.php?page=premium&amp;openDetail=3"
-        class="tooltipHTML {{ $short_time_officier_admiral }} {{ $img_premium_officier_admiral }} admiral js_hideTipOnMobile "
+        class="tooltipHTML {{ $end_premium_officier_admiral }} {{ $img_premium_officier_admiral }} admiral js_hideTipOnMobile "
         title="{{ $of_hire_admiral }}|{{ $add_premium_officier_admiral }}">
         <img src="{{ $img_path . 'layout/pixel.gif' }}" width="30" height="30" />
     </a>
     <a href="game.php?page=premium&amp;openDetail=4"
-        class="tooltipHTML {{ $short_time_officier_engineer }} {{ $img_premium_officier_engineer }} engineer js_hideTipOnMobile "
+        class="tooltipHTML {{ $end_premium_officier_engineer }} {{ $img_premium_officier_engineer }} engineer js_hideTipOnMobile "
         title="{{ $of_hire_engineer }}|{{ $add_premium_officier_engineer }}">
         <img src="{{ $img_path . 'layout/pixel.gif' }}" width="30" height="30" />
     </a>
     <a href="game.php?page=premium&amp;openDetail=5"
-        class="tooltipHTML {{ $short_time_officier_geologist }} {{ $img_premium_officier_geologist }} geologist js_hideTipOnMobile "
+        class="tooltipHTML {{ $end_premium_officier_geologist }} {{ $img_premium_officier_geologist }} geologist js_hideTipOnMobile "
         title="{{ $of_hire_geologist }}|{{ $add_premium_officier_geologist }}">
         <img src="{{ $img_path . 'layout/pixel.gif' }}" width="30" height="30" />
     </a>
     <a href="game.php?page=premium&amp;openDetail=6"
-        class="tooltipHTML {{ $short_time_officier_technocrat }} {{ $img_premium_officier_technocrat }}  technocrat js_hideTipOnMobile "
+        class="tooltipHTML {{ $end_premium_officier_technocrat }} {{ $img_premium_officier_technocrat }}  technocrat js_hideTipOnMobile "
         title="{{ $of_hire_technocrat }}|{{ $add_premium_officier_technocrat }}">
         <img src="{{ $img_path . 'layout/pixel.gif' }}" width="30" height="30" />
     </a>
 </div>
-{{ $short_time_officier_technocrat }}
